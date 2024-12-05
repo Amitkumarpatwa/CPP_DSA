3469606f7031751fad35dd5b398a41ad566535ca
AmitKumarPatwa <amitpatwa80040@gmail.com>
2024-12-05 15:54:35 +0530

    h

--- 5recursion/3fibnacci.cpp
+++ 5recursion/3fibnacci.cpp
@@ -1,10 +1,20 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main ()
-{
-  int n;
-  cout<<"Enter a number:";
-  cin>>n;
-  
-  return 0;
+int fib(int x) {
+   if((x==1)||(x==0)) {
+      return(x);
+   }else {
+      return(fib(x-1)+fib(x-2));
+   }
+}
+int main() {
+   int x , i=0;
+   cout << "Enter the number of terms of series : ";
+   cin >> x;
+   cout << "\nFibonnaci Series : ";
+   while(i < x) {
+      cout << " " << fib(i);
+      i++;
+   }
+   return 0;
 }
